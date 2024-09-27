07107743a71f0defb3fb654a2876b953641930c5
strajaDev <straja.dev@hotmail.com>
2024-09-27 23:52:14 +0200

    add redux toolkit and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import styles from "./page.module.css";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h2>Test</h2>
+      <div className="flex justify-center items-center h-screen">
+        <Button variant="default">Default Button</Button>
+        <Button variant="outline">Outlined Button</Button>
+        <Button variant="ghost">Ghost Button</Button>
+        <Button variant="link">Link Button</Button>
+      </div>
     </div>
   );
 }
