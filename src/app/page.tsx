526ab28a0aada0f1c5f53f96b90090610bc73119
StrajaDev <straja.dev@hotmail.com>
2024-09-27 23:06:02 +0200

    tst commit windows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-    <h2>Test</h2>
+      <h2>Test</h2>
     </div>
   );
 }
